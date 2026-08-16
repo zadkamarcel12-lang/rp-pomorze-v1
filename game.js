4dbfeec9b859f304f299e8e7ef438a8fba4d73e2
odi1125 <nodi71544@gmail.com>
2026-08-16 10:01:02 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
-// Połączenie z serwerem (podmień na adres swojego serwera, np. na Render.com)
+// Podmień na swój adres z Rendera!
 const socket = io("https://rp-pomorze-serwer.onrender.com");
 
 const inniGracze = {};
 
-// Obsługa przycisku zaloguj
+// Obsługa przycisku logowania
 document.getElementById("btnZaloguj").addEventListener("click", () => {
     const nick = document.getElementById("inputLogin").value.trim();
     
@@ -13,61 +13,66 @@ document.getElementById("btnZaloguj").addEventListener("click", () => {
         document.getElementById("uiGry").classList.remove("ukryty");
         document.getElementById("nazwaZalogowanego").innerText = nick;
 
-        // Powiadamiamy serwer o zalogowaniu gracza
-        socket.emit("doluaczDoGry", { nick: nick, x: gracz.x, y: gracz.y });
+        // Przypisujemy nick do obiektu gracza (zakładając, że obiekt 'gracz' jest zdefiniowany w gracz.js)
+        if (typeof gracz !== 'undefined') {
+            gracz.nick = nick;
+        }
+
+        // Informujemy serwer o dołączeniu do gry
+        const posX = typeof gracz !== 'undefined' ? gracz.x : 100;
+        const posY = typeof gracz !== 'undefined' ? gracz.y : 100;
+        socket.emit("joinGame", { nick: nick, x: posX, y: posY });
+    } else {
+        alert("Wpisz nazwę gracza!");
     }
 });
 
-// === NASŁUCHIWANIE INNYCH GRACZY ===
+// === NASŁUCHIWANIE ZDARZEŃ SIECIOWYCH ===
 
-// Odbieramy listę obecnych graczy
-socket.on("aktualniGracze", (gracze) => {
-    Object.keys(gracze).forEach((id) => {
+socket.on("currentPlayers", (players) => {
+    Object.keys(players).forEach((id) => {
         if (id !== socket.id) {
-            inniGracze[id] = gracze[id];
+            inniGracze[id] = players[id];
         }
     });
 });
 
-// Nowy gracz dołącza
-socket.on("nowyGracz", (daneGracza) => {
-    inniGracze[daneGracza.id] = daneGracza;
+socket.on("newPlayer", (playerInfo) => {
+    inniGracze[playerInfo.id] = playerInfo;
 });
 
-// Inny gracz się poruszył
-socket.on("graczSiePoruszyl", (daneGracza) => {
-    if (inniGracze[daneGracza.id]) {
-        inniGracze[daneGracza.id].x = daneGracza.x;
-        inniGracze[daneGracza.id].y = daneGracza.y;
+socket.on("playerMoved", (playerInfo) => {
+    if (inniGracze[playerInfo.id]) {
+        inniGracze[playerInfo.id].x = playerInfo.x;
+        inniGracze[playerInfo.id].y = playerInfo.y;
     }
 });
 
-// Gracz wyszedł z gry
-socket.on("graczOdlaczony", (id) => {
+socket.on("playerDisconnected", (id) => {
     delete inniGracze[id];
 });
 
-// === WYSYŁANIE RUCHU ===
-// W swojej pętli gry (tam gdzie zmieniasz gracz.x i gracz.y po wciśnięciu klawiszy):
-function obslugaRuchu() {
-    // np. gracz.x += predkosc;
-    
-    // Po zmianie pozycji wysyłamy ją na serwer:
-    socket.emit("ruchGracza", { x: gracz.x, y: gracz.y });
+// === FUNKCJA WYSYŁAJĄCA RUCH ===
+// Wywołuj tę funkcję w swojej pętli gry, gdy gracz się porusza:
+function wyslijRuch() {
+    if (typeof gracz !== 'undefined' && socket.connected) {
+        socket.emit("playerMovement", { x: gracz.x, y: gracz.y });
+    }
 }
 
-// === RYSOWANIE INNYCH GRACZY NA CANVAS ===
+// === FUNKCJA RYSUJĄCA INNYCH GRACZY ===
+// Wywołuj tę funkcję w swojej głównej funkcji rysującej (np. w pętli renderowania na canvasie):
 function rysujInnychGraczy(ctx) {
     Object.keys(inniGracze).forEach((id) => {
         const g = inniGracze[id];
         
-        // Rysujemy postać drugiego gracza
+        // Rysowanie kwadratu/postaci drugiego gracza
         ctx.fillStyle = "red";
         ctx.fillRect(g.x, g.y, 32, 32);
 
-        // Rysujemy nick nad postacią
+        // Rysowanie nicku nad postacią
         ctx.fillStyle = "white";
-        ctx.font = "12px Arial";
+        ctx.font = "12px sans-serif";
         ctx.textAlign = "center";
         ctx.fillText(g.nick || "Druh", g.x + 16, g.y - 6);
     });
